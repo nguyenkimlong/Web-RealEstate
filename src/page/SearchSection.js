9e43e5650c981b49e8aa8cf152a9446c78369f45
kimlong.nguyen <kimlong.nguyen@aestechgroup.com>
2021-05-05 17:18:03 +0700

    commit all

--- src/page/SearchSection.js
+++ src/page/SearchSection.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+
 import { Button } from 'react-bootstrap';
 function SearchSection() {
     return (
         <div>
+            <Button> Click Search </Button>
 SearchSection
         </div>
     )
