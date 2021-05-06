9cce8a093f6a6ff53a6060bb5c5e46570fcf2033
kimlong.nguyen <kimlong.nguyen@aestechgroup.com>
2021-05-06 11:27:09 +0700

    commit update

--- src/page/SearchResult.js
+++ src/page/SearchResult.js
@@ -1,17 +1,38 @@
 
 
 function SearchResult() {
-    return (
-        
-        
-<section className="features-icons bg-light text-center">
-  <div className="container">
-    <div className="row">
-        Grid
-    </div>
-  </div>
-</section>
-    )
+  return (
+    <section className="features-icons bg-light text-center">
+      <div className="container">
+        <div className="row">
+          <table className="table">
+            <thead>
+              <tr>
+                <td>Image</td>
+                <td>Rong</td>
+                <td>Dai</td>
+                <td>So phong ngu</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>ảnh 1</td>
+                <td>20</td>
+                <td>30</td>
+                <td>2</td>
+              </tr>
+              <tr>
+                <td>ảnh 2</td>
+                <td>30</td>
+                <td>35</td>
+                <td>3</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 
--- src/page/SearchSection.js
+++ src/page/SearchSection.js
@@ -1,49 +1,98 @@
 
 import SearchResult from './SearchResult.js';
+import { Form } from 'react-bootstrap';
+import React, { Component } from 'react';
 
+class SearchSection extends Component {
 
-function SearchSection() {
-    return (
-        <div>
+    constructor(props) {
+        super(props);
+        this.state = {
+            selectedProperty: "",
+            selectedBed: ""
+        }
+    }
 
-            <header className="masthead text-white text-center">
-                <div className="overlay"></div>
-                <div className="container">
-                    <div className="row">
-                        <div className="col-xl-9 mx-auto">
-                            <h1 className="mb-5">Build a landing page for your business or project and generate more leads!</h1>
-                        </div>
-                        <div className="col-md-10 col-lg-8 col-xl-7 mx-auto">
-                            <form>
-                                <div className="form-row">
-                                    <div className="col-12 col-md-9 mb-2 mb-md-0"><input className="form-control form-control-lg" type="email"
-                                        placeholder="Enter your email..." /></div>
-                                    <div className="col-12 col-md-3">
-                                        <button className="btn btn-block btn-lg btn-primary" type="submit">Search</button>
+    onChangeProperty(event) {
+        this.setState({ selectedProperty: event.target.value });
+        console.log(this.state.selectedProperty);
+    }
+
+    onChangeBeds(event) {
+        this.setState({ selectedBed: event.target.value });
+        console.log(this.state.selectedBed);
+    }
+
+    render() {
+        return (
+            <div>
+                <header className="masthead text-white text-center">
+                    <div className="overlay"></div>
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-xl-9 mx-auto">
+                                <h1 className="mb-5">Find Designs</h1>
+                            </div>
+                            <div className="col-lg-10 mx-auto">
+                                <form role="form">
+                                    <div className="form-row">
+                                        <div className="col-3">
+                                            <Form.Control as="select" defaultValue={''} onChange={this.onChangeProperty.bind(this)}>
+                                                <option value="">Kiểu Nhà</option>
+                                                <option value="House">House</option>
+                                                <option value="ApartmentUnit">Apartment & Unit</option>
+                                                <option value="Villa">Villa</option>
+                                                <option value="Land">Land</option>
+                                            </Form.Control>
+                                        </div>
+                                        <div className="col-3">
+                                            <Form.Control as="select" defaultValue={''} onChange={this.onChangeBeds.bind(this)}>
+                                                <option value="">Số Phòng Ngủ</option>
+                                                <option value="1">1 Phòng Ngủ</option>
+                                                <option value="2">2 Phòng Ngủ</option>
+                                                <option value="3">3 Phòng Ngủ</option>
+                                                <option value="4">4 Phòng Ngủ</option>
+                                                <option value="5">5 Phòng Ngủ</option>
+                                            </Form.Control>
+                                        </div>
+                                        <div className="col-3">
+                                            <Form.Control type="number" placeholder="Chiều Rộng" />
+                                        </div>
+                                        <div className="col-3">
+                                            <Form.Control type="number" placeholder="Chiều Dài" />
+                                        </div>
+                                        <div className="col-3">
+                                            <Form.Group controlId="formBasicRange">
+                                                <Form.Label>Số Tầng</Form.Label>
+                                                <Form.Control as="select">
+                                                    <option value="1">1 Tầng</option>
+                                                    <option value="2">2 Tầng</option>
+                                                    <option value="3">3 Tầng</option>
+                                                    <option value="4">4 Tầng</option>
+                                                    <option value="5">5 Tầng</option>
+                                                </Form.Control>
+                                            </Form.Group>
+
+                                        </div>
                                     </div>
-                                    <div className="col-12  col-md-3">
-                                        <select class="select" multiple>
-                                            <option value="1">One</option>
-                                            <option value="2">Two</option>
-                                            <option value="3">Three</option>
-                                            <option value="4">Four</option>
-                                            <option value="5">Five</option>
-                                            <option value="6">Six</option>
-                                            <option value="7">Seven</option>
-                                            <option value="8">Eight</option>
-                                        </select>
+                                    <div className="form-row">
+                                        <div className="col-12 col-md-3">
+                                            <button className="btn btn-block btn-lg btn-primary" type="button">Search</button>
+                                        </div>
                                     </div>
-                                </div>
-                            </form>
+                                </form>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </header>
-            <SearchResult />
-        </div>
+                </header>
+                <SearchResult />
+            </div>
 
 
-    )
+        )
+
+    }
+
 }
 
 export default SearchSection;
